877b383cc04363ef877d89a5b8bd53a16ce0f0a2
Juliano Rohde <julianorohde@hotmail.com>
2024-08-15 17:47:33 -0400

    Configure Sidekiq integration with Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users
 
   root 'pages#index'
   get 'pages/index'
+  mount Sidekiq::Web => '/sidekiq'
 end
