661b4851eb7587855315fd2ce7a30539c6a63587
Juliano Rohde <julianorohde@hotmail.com>
2024-08-15 00:07:32 -0400

    Revert "Configure ApplicationController to permit all parameters for User model in Devise"
    This reverts commit 484e451ebf7bbf2f46d19349b3bd1a6a606ec239.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
-# frozen_string_literal: true
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name cpf email password password_confirmation])
-  end
 end
