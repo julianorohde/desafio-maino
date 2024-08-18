8eb929529f6c5b5b9f94212fd5fc4da50cb5d031
Juliano Rohde <julianorohde@hotmail.com>
2024-08-18 10:42:55 -0400

    Configure Rails to use pt-BR locale with i18n

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module DesafioMaino
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # Configure Rails to load translations from the locales directory and set the default locale to pt-BR
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+    config.i18n.default_locale = :'pt-BR'
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
