763e22f32fec257e2e483b550372127b6ed34967
Juliano Rohde <julianorohde@hotmail.com>
2024-08-18 21:35:59 -0400

    Set AWS S3 configurations only for the production environment

--- config/initializers/aws_client.rb
+++ config/initializers/aws_client.rb
@@ -1,9 +1,11 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-Aws.config.update(
-  endpoint: 'https://' + ENV["STACKHERO_MINIO_HOST"],
-  access_key_id: ENV["STACKHERO_MINIO_ROOT_ACCESS_KEY"],
-  secret_access_key: ENV["STACKHERO_MINIO_ROOT_SECRET_KEY"],
-  force_path_style: true,
-  region: 'us-east-1'
-)
+if Rails.env.production?
+  Aws.config.update(
+    endpoint: 'https://' + ENV["STACKHERO_MINIO_HOST"],
+    access_key_id: ENV["STACKHERO_MINIO_ROOT_ACCESS_KEY"],
+    secret_access_key: ENV["STACKHERO_MINIO_ROOT_SECRET_KEY"],
+    force_path_style: true,
+    region: 'us-east-1'
+  )
+end
